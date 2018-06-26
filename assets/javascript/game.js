07f6a768c79942c1b71e825ebdb9bd53a814a323
Unknown <kjk464@humboldt.edu>
2018-06-25 22:06:57 -0700

    create skeleton
    add game divs to index, copy over css, prototype js

---
+++ assets/javascript/game.js
@@ -0,0 +1,55 @@
+var character = {
+    Character: function(hero) {
+        this.name = hero.name;
+        this.hp = hero.hp;
+        this.ap = hero.ap;
+        this.cap = hero.cap;
+    },
+    dennis: {
+        name: "Dennis Reynolds",
+        hp: 100,
+        ap: 10,
+        cap: 12
+    },
+    dee: {
+        name: "Dee Reynolds",
+        hp: 100,
+        ap: 10,
+        cap: 12
+    },
+    vader: {
+        name: "Mac",
+        hp: 100,
+        ap: 10,
+        cap: 12
+    },
+    maul: {
+        name: "Charlie Kelly",
+        hp: 100,
+        ap: 10,
+        cap: 12
+    }
+}
+
+var Game = {
+    initGame: function() {
+        /*
+        - grab all game divs w/ jQuery
+        - give each character an on(click, func) where func 
+            - 
+        */
+    },
+    chooseHero: function() {
+        // TODO: jQuery onclick
+        // if each 
+    },
+    chooseEnemy: function() {
+
+    },
+    doBattle: function() {
+
+    },
+    playGame: function() {
+
+    }
+}
